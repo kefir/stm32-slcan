eef2bc886952d9a37e8b005aed434f8070a3413a
Sergey Tkachenko <sergey.iray@yandex.ru>
2021-10-23 05:42:35 +0300

    switched to USART1

--- pinout.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <libopencm3/stm32/can.h>
-#include <libopencm3/stm32/flash.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/usart.h>
-
-#define UART_PORT RCC_GPIOA
